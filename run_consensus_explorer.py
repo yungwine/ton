2fa2e25fba0e7ed219db7886adfd650cb327fa7a
yungwine <cyrbatoff@gmail.com>
2026-01-15 19:16:42 +1100

    fix runner process kill

--- run_consensus_explorer.py
+++ run_consensus_explorer.py
@@ -1,10 +1,20 @@
 import os
+import signal
 import subprocess
 import time
 from pathlib import Path
 
 cur_dir = None
 process = None
+
+
+def stop_process(proc: subprocess.Popen):
+    try:
+        os.killpg(proc.pid, signal.SIGTERM)
+        proc.wait(timeout=10)
+    except subprocess.TimeoutExpired:
+        os.killpg(proc.pid, signal.SIGKILL)
+
 
 while True:
     time.sleep(10)
@@ -19,8 +29,9 @@ while True:
         cur_dir = new_dirs[-1]
         print(f'running explorer for new dir {cur_dir}')
         if process:
-            process.kill()
+            stop_process(process)
 
         logs = [str(cur_dir / f) for f in os.listdir(cur_dir)]
 
-        process = subprocess.Popen(['uv', 'run', 'consensus_explorer', '--host', '0.0.0.0', '--logs', *logs])
+        process = subprocess.Popen(['uv', 'run', 'consensus_explorer', '--host', '0.0.0.0', '--logs', *logs],
+                                   start_new_session=True)
